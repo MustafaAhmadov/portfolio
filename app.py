d2c5040d3d299cadeda523928732707849210c98
MustafaAhmadov <88622101+MustafaAhmadov@users.noreply.github.com>
2024-03-10 15:14:07 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, jsonify, request
+from simpleai.search import astar, SearchProblem
+
+app = Flask(__name__)
+
+class SimpleProblem(SearchProblem):
+    def actions(self, state):
+        # Define actions based on state
+        pass
+
+    def result(self, state, action):
+        # Define result of an action
+        pass
+
+    def is_goal(self, state):
+        # Define goal state
+        pass
+
+    def heuristic(self, state):
+        # Define heuristic for A*
+        pass
+
+@app.route('/solve', methods=['POST'])
+def solve_problem():
+    data = request.json
+    problem_instance = SimpleProblem(initial_state=data['initial_state'])
+    result = astar(problem_instance)
+    return jsonify({'solution': result.state, 'path': [step[1] for step in result.path() if step[1] is not None]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+from flask import Flask, jsonify, request
+from simpleai.search import SearchProblem, astar
+
+app = Flask(__name__)
+
+# A simple search problem that reverses a string
+class ReverseStringProblem(SearchProblem):
+    def actions(self, state):
+        # The only 'action' in our simple problem is to reverse the string
+        return ['reverse']
+
+    def result(self, state, action):
+        # The result of the 'reverse' action is simply to reverse the state (string)
+        if action == 'reverse':
+            return state[::-1]
+
+    def is_goal(self, state):
+        # We won't actually use this for our simple problem
+        pass
+
+    def value(self, state):
+        # Value function for local search (not used in this example)
+        pass
+
+@app.route('/solve', methods=['POST'])
+def solve_problem():
+    # Extract parameters from the request
+    data = request.json
+    input_string = data.get('input', '')
+
+    # Setup and solve the problem
+    problem_instance = ReverseStringProblem(initial_state=input_string)
+    result = problem_instance.result(input_string, 'reverse')  # Directly use the result for simplicity
+
+    # Return the result as JSON
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
